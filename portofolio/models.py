27ca6fc5edb783366e5cf3cb94d3fb3c98f68735
PJFonseca <pedro@fonseca.com.pt>
2026-04-06 14:07:35 +0100

    INIT Models - Discipline

--- portofolio/models.py
+++ portofolio/models.py
@@ -4,5 +4,19 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=200)
-    presentation = models.TextField()
+    description = models.TextField()
     ects = models.IntegerField()
+    background_color = models.CharField(max_length=20)
+    text_color = models.CharField(max_length=20)
+    icon = models.CharField(max_length=100)
+
+class Discipline(models.Model):
+    # One Discipline exists in one course, I'm assuming every discipline is tailored for that course only
+    courseid = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='courses')
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    ects = models.IntegerField()
+    background_color = models.CharField(max_length=20)
+    text_color = models.CharField(max_length=20)
+    icon = models.CharField(max_length=100)
+
