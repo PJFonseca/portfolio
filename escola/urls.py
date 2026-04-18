038bdde9ec15e4b5c355c449633e3d27dad4f260
Pedro Fonseca <pedro+github@fonseca.com.pt>
2026-04-18 15:09:57 +0000

    Teachers

--- escola/urls.py
+++ escola/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.cursos_view, name="cursos"),
     path('cursos/', views.cursos_view, name="cursos"),
+    path('professores/', views.professores_view, name="professores"),
 ]
